3a72a2fa6fdb51bf1ae5fafe55033868fc0e35c0
pedroalles <pedroalles@gmail.com>
2021-07-28 03:41:35 -0300

    Login

--- my-app/infra/getMarket.js
+++ my-app/infra/getMarket.js
@@ -67,27 +67,15 @@ export async function loginSteam({ login, pass, sg }) {
 
   await page.setViewport({ width: width, height: height });
   await page.goto('https://steamcommunity.com/login/home/?goto=market%2F');
-  // await page.$eval('#input_username', el => el.value = login)
-  // await page.$eval('#input_password', el => el.value = pass)
+
   await page.type('#input_username', login);
   await page.type('#input_password', pass);
-
   await page.click('#login_btn_signin > button');
-  // await page.click('#submit');
 
   await page.waitForSelector('#twofactorcode_entry');
-  // await page.$eval('#twofactorcode_entry', el => el.value = sg)
-  // await page.evaluate(() => document.querySelector('#twofactorcode_entry').click())
-
-  // await page.$eval(selector, (element, attribute) => element.getAttribute(attribute), attribute);
-  // await page.$eval('#twofactorcode_entry', (el, param) => el.innerText = param, sg);
-  // await page.hover('#twofactorcode_entry')
-  // await page.click('#twofactorcode_entry')
   await page.waitForSelector('#twofactorcode_entry', { visible: true })
   await page.type('#twofactorcode_entry', sg);
   await page.click('#login_twofactorauth_buttonset_entercode > div.auth_button.leftbtn');
-  // await page.evaluate(() => document.querySelector('#login_twofactorauth_buttonset_entercode > div.auth_button.leftbtn').click())
-  // await page.click('#twofactorcode_entry');
 };
 
 
@@ -96,37 +84,26 @@ export async function getMarketInfos() {
 
   await page.waitForSelector('#marketWalletBalance', { visible: true })
   // await page.setViewport({ width: width, height: height });
-
   // await page.waitFor(2000);
   // await page.goto('https://steamcommunity.com/market/');
 
-  if (isDev) {
-
-    const pageContent = await page.evaluate(() => {
-      const getInfos = (el) => ({
-        "Sticker Name": el.innerText,
-        "Sticker Url": el.getAttribute('href'),
-      });
-      return {
-        userName: document.querySelector('#account_pulldown').innerText,
-        walletBalance: document.querySelector('#header_wallet_balance').innerText,
-        listings: [...document.querySelectorAll('span[id*="mylisting_"] > a')].map(getInfos),
-        orders: [...document.querySelectorAll('span[id*="mbuyorder_"] > a')].map(getInfos),
-      }
-    })
-
-    await _browser.close()
-    _page = null
-    _browser = null
-    console.log(pageContent);
-    return pageContent
-  }
-
-  const pageContent = await page.title()
+  const pageContent = await page.evaluate(() => {
+    const getInfos = (el) => ({
+      "Sticker Name": el.innerText,
+      "Sticker Url": el.getAttribute('href'),
+    });
+    return {
+      userName: document.querySelector('#account_pulldown').innerText,
+      walletBalance: document.querySelector('#header_wallet_balance').innerText,
+      listings: [...document.querySelectorAll('span[id*="mylisting_"] > a')].map(getInfos),
+      orders: [...document.querySelectorAll('span[id*="mbuyorder_"] > a')].map(getInfos),
+    }
+  })
 
   await _browser.close()
   _page = null
   _browser = null
   console.log(pageContent);
-  return { pageContent }
+  return pageContent
+
 }
