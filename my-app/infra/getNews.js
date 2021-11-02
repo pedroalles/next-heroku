d118eae6dc0dfc7dfb36768d96963170e5b4a27a
pedroalles <pedroalles@gmail.com>
2021-11-02 07:53:19 -0300

    Change api core

---
+++ my-app/infra/getNews.js
@@ -0,0 +1,71 @@
+import puppeteer from 'puppeteer'
+
+async function getOptions() {
+  const isDev = process.env.NODE_ENV == 'development'
+  let options;
+
+  const chromeExecPaths = {
+    win32: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
+    linux: '/usr/bin/google-chrome',
+    darwin: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+  }
+
+  const exePath = chromeExecPaths[process.platform]
+
+  if (isDev) {
+    options = {
+      args: [],
+      executablePath: exePath,
+      headless: true
+    }
+  } else {
+    options = {
+      args: ["--no-sandbox"],
+      headless: true
+    }
+  }
+
+  return options
+}
+
+let _page = null
+let _browser = null
+
+async function getPage() {
+  if (_page) {
+    return _page
+  }
+  if (_browser) {
+    return _browser
+  }
+
+  const options = await getOptions()
+  _browser = await puppeteer.launch(options)
+
+  _page = await _browser.newPage()
+
+  return _page
+}
+
+export async function getNews({ width, height } = { width: 800, height: 800 }) {
+  const page = await getPage();
+  const url = 'https://ge.globo.com/rs/futebol/times/gremio/';
+  await page.setViewport({ width, height });
+  await page.goto(url);
+
+  const result = await page.evaluate(() => {
+    const posts = []
+    document.querySelectorAll('div.feed-post-body-title.gui-color-primary.gui-color-hover > div > a')
+      .forEach((post) => posts.push({
+        title: post.textContent,
+        url: post.getAttribute('href')
+      }))
+    return posts
+  })
+
+  await _browser.close()
+  _page = null
+  _browser = null
+
+  return result;
+}
