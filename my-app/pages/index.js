4684ee41e6b1f4b2bb7eeeb6701bd3665136ca6b
pedroalles <pedroalles@gmail.com>
2021-11-03 08:31:51 -0300

    Fix news source

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,23 +1,28 @@
 import styles from '../styles/Home.module.css'
 import { useEffect, useState } from 'react';
 
-export default function Home() {
+export default function Home({ data }) {
 
   const [news, setNews] = useState([]);
   const [isLoading, setIsLoading] = useState(false)
 
   useEffect(() => {
     getNews()
+    // setNews(data)
   }, [])
 
   const getNews = async () => {
     setIsLoading(true)
-    const res = await fetch(`http://localhost:${process.env.PORT || 3000}/api/news2`)
+    const url = `http://localhost:${process.env.PORT || 3000}/api/news2`
+    // const url = 'http://gremio-news.herokuapp.com/api/news2'
+    console.log('url', url);
+    const res = await fetch(url)
     const data = await res.json()
     console.log(data);
     setNews(data)
     setIsLoading(false)
   }
+
 
   if (isLoading) {
     return <p>Buscando as quentinhas do tricolor...</p>
@@ -39,3 +44,16 @@ export default function Home() {
     </div>
   )
 }
+
+// Home.getInitialProps = async () => {
+//   console.log('fetching data...');
+//   const url = `http://localhost:${process.env.PORT || 3000}/api/news`
+//   // const url = 'https://gremio-news.herokuapp.com/api/news2'
+//   // const url = 'http://localhost:3000/api/news2'
+//   const res = await fetch(url)
+//   const data = await res.json()
+//   console.log(data.length);
+//   return {
+//     data,
+//   }
+// }
