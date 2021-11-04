6e4bcb8dc43baabbd0c9027741602a086e02df8e
pedroalles <pedroalles@gmail.com>
2021-11-04 07:11:03 -0300

    Testing port scope

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,11 +1,7 @@
 import styles from '../styles/Home.module.css'
 import { useEffect, useState } from 'react';
-import ReactAudioPlayer from 'react-audio-player';
 
-const port = process.env.PORT || 3000;
-console.log('port on index.js', port);
-
-export default function Home({ data }) {
+export default function Home({ port }) {
 
   const [news, setNews] = useState([]);
 
@@ -14,30 +10,47 @@ export default function Home({ data }) {
   // const audio_url = "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"
 
   useEffect(() => {
-    setNews(data)
+    console.log('port on useEffect', port);
+    // setNews(data)
+    fetchData()
   }, [])
+
+  const fetchData = async () => {
+    const url = `http://localhost:${port}/api/news2`
+    const res = await fetch(url)
+    const data = await res.json()
+    console.log(data);
+  }
 
   return (
     <div className={styles.container}>
-      {news.map(el => (
+      {/* {news.map(el => (
         <div key={el.url} className={styles.card}>
           <a href={el.url} target="_blank" rel="noreferrer">
             <h6 className={styles.site}>{el.site}</h6>
             <h4 className={styles.title}>{el.title}</h4>
           </a>
         </div>
-      ))}
+      ))} */}
     </div>
   )
 }
 
+// Home.getInitialProps = async () => {
+//   const port = process.env.PORT || 3000;
+//   console.log('port on getInitialProps', port);
+//   const url = `http://localhost:${port}/api/news2`
+//   const res = await fetch(url)
+//   const data = await res.json()
+//   return {
+//     data,
+//   }
+// }
+
 Home.getInitialProps = async () => {
   const port = process.env.PORT || 3000;
   console.log('port on getInitialProps', port);
-  const url = `http://localhost:${port}/api/news2`
-  const res = await fetch(url)
-  const data = await res.json()
   return {
-    data,
+    port,
   }
 }
