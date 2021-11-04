9cb49b931ea2ab204598d65e5afeb1bcb36580a1
pedroalles <pedroalles@gmail.com>
2021-11-04 07:59:48 -0300

    Testing port scope

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -4,42 +4,38 @@ import { useEffect, useState } from 'react';
 export default function Home({ port }) {
 
   const [news, setNews] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   // const audio_url = "https://protettordelinks.com/wp-content/audiosparazap/hino_do_gremio.mp3"
 
   // const audio_url = "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"
 
   useEffect(() => {
-    console.log('port on useEffect', port);
-    // setNews(data)
     fetchData()
-    fetchData2()
   }, [])
 
   const fetchData = async () => {
+    setIsLoading(true);
     const url = `https://gremio-news.herokuapp.com/api/news2`
     const res = await fetch(url)
     const data = await res.json()
+    setNews(data)
+    setIsLoading(false);
     console.log(data);
   }
 
-  const fetchData2 = async () => {
-    const url = `https://pokeapi.co/api/v2/pokemon/ditto`
-    const res = await fetch(url)
-    const data = await res.json()
-    console.log(data);
-  }
+  if (isLoading) return <p>Buscando as quentinhas do tricolor...</p>
 
   return (
     <div className={styles.container}>
-      {/* {news.map(el => (
+      {news.map(el => (
         <div key={el.url} className={styles.card}>
           <a href={el.url} target="_blank" rel="noreferrer">
             <h6 className={styles.site}>{el.site}</h6>
             <h4 className={styles.title}>{el.title}</h4>
           </a>
         </div>
-      ))} */}
+      ))}
     </div>
   )
 }
@@ -55,10 +51,10 @@ export default function Home({ port }) {
 //   }
 // }
 
-Home.getInitialProps = async () => {
-  const port = process.env.PORT || 3000;
-  console.log('port on getInitialProps', port);
-  return {
-    port,
-  }
-}
+// Home.getInitialProps = async () => {
+//   const port = process.env.PORT || 3000;
+//   console.log('port on getInitialProps', port);
+//   return {
+//     port,
+//   }
+// }
