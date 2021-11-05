a0fa7625b58efb4bb408f94504161f6337543772
pedroalles <pedroalles@gmail.com>
2021-11-05 19:30:59 -0300

    Add search bar

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,10 +1,13 @@
 import styles from '../styles/Home.module.css'
 import { useEffect, useState } from 'react';
 
+const isProd = process.NODE_ENV === 'production'
+
 export default function Home() {
 
   const [news, setNews] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [filter, setFilter] = useState('');
 
   useEffect(() => {
     fetchData()
@@ -12,13 +15,22 @@ export default function Home() {
 
   const fetchData = async () => {
     setIsLoading(true);
-    const url = `https://gremio-news.herokuapp.com/api/news`
+
+    let url;
+    if (isProd) {
+      url = `https://gremio-news.herokuapp.com/api/news2`
+    } else {
+      url = `http://localhost:3000/api/news2`
+    }
+
     const res = await fetch(url)
     const data = await res.json()
     setNews(data)
     setIsLoading(false);
-    console.log(data);
   }
+
+  const filteredData = news.filter(el => el.title.toLowerCase().includes(filter.toLowerCase())
+    || el.site.toLowerCase().includes(filter.toLowerCase()))
 
   if (isLoading) return (
     <div className={styles.loading}>
@@ -29,14 +41,22 @@ export default function Home() {
 
   return (
     <div className={styles.container}>
-      {news.map(el => (
+      <input
+        className={styles.search}
+        type="text"
+        placeholder="Search"
+        value={filter}
+        onChange={(e) => setFilter(e.target.value)}
+      />
+      {filteredData.length > 0 ? filteredData.map(el => (
         <div key={el.url} className={styles.card}>
           <a href={el.url} target="_blank" rel="noreferrer">
             <h6 className={styles.site}>{el.site}</h6>
             <h4 className={styles.title}>{el.title}</h4>
           </a>
         </div>
-      ))}
+      ))
+        : news.length > 0 && <h2 className={styles.noresults}>Nenhum resultado.</h2>}
     </div>
   )
 }
